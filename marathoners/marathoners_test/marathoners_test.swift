4607d692bbda943a958c230a0e53f091dbed9e3c
seizze <cwcwc3@gmail.com>
2020-01-08 10:24:56 +0900

    Initialize marathoners project

---
+++ marathoners/marathoners_test/marathoners_test.swift
@@ -0,0 +1,20 @@
+//
+//  marathoners_test.swift
+//  marathoners_test
+//
+//  Created by Chaewan Park on 2020/01/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import XCTest
+
+class marathoners_test: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
