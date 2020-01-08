1279b68681398dfd7f8e805f43af312216a05c34
seizze <cwcwc3@gmail.com>
2020-01-08 14:27:24 +0900

    Initialize reverse integer project

---
+++ reverse_integer/reverse_integer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  reverse_integer
+//
+//  Created by Chaewan Park on 2020/01/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ reverse_integer/reverse_integer_test/reverse_integer_test.swift
@@ -0,0 +1,33 @@
+//
+//  reverse_integer_test.swift
+//  reverse_integer_test
+//
+//  Created by Chaewan Park on 2020/01/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import XCTest
+
+class reverse_integer_test: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
