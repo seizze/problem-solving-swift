1b285622e40f3407bb8d1672beb40dc1f5823535
seizze <cwcwc3@gmail.com>
2020-01-15 10:40:40 +0900

    Initialize kth number project

---
+++ kth_number/kth_number/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  kth_number
+//
+//  Created by Chaewan Park on 2020/01/15.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
