60165d0e250614308872d592d291f4fa02db2d45
seizze <cwcwc3@gmail.com>
2020-01-22 11:41:23 +0900

    Initialize happy number project

---
+++ happy_number/happy_number/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  happy_number
+//
+//  Created by Chaewan Park on 2020/01/22.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
