b8c5234db0afae6b5b63e284cec9945b6df64225
seizze <cwcwc3@gmail.com>
2020-07-08 14:46:26 +0900

    Initialize project for hackerrank problem solving

---
+++ HackerRank/HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Chaewan Park on 2020/07/08.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
